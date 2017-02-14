011d525678efa6abd0bf623f79c62518ee11bb33
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-02-14 14:44:44 +1100

    fix server routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.post('/', function(request, response) {
   response.render('index');
 });
 
