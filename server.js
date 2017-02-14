1e1a0cd83e32f18ee29d4276092be6b8a2a50254
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-02-14 16:06:35 +1100

    filepath error fix for heroku deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port_number = server.listen(process.env.PORT || 3000);
 
 server.set('views', './');
 server.set('view engine', 'ejs');
-server.use(express.static(path.join(__dirname, 'app')));
+server.use(express.static(path.join(__dirname, './')));
 
 server.get('/', function(request, response) {
   response.render('index');
